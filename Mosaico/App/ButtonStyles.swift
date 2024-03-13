e425b5fb67038403b42f8e89842956ef0d7641a6
Mark Mckelvie <markmck1@gmail.com>
2024-03-13 01:44:59 -0400

    Adds button style and color for new game button

---
+++ Mosaico/App/ButtonStyles.swift
@@ -0,0 +1,22 @@
+//
+//  ButtonStyles.swift
+//  Mosaico
+//
+//  Created by Mark Mckelvie on 3/13/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct StartOverButtonStyle: ButtonStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(.vertical, kMediumPadding)
+            .padding(.horizontal, kLargePadding)
+            .background(Color.mGreen)
+            .foregroundStyle(.white)
+            .clipShape(Capsule())
+            .font(AppFonts.helveticaNeue(ofSize: kRegularFontSize))
+    }
+}
